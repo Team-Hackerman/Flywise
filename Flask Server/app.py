5bf8316fd395487707de78df14a88e5951aa9fcc
Harsh Jain <jain.hn@somaiya.edu>
2022-10-16 01:52:28 +0530

    Server main code

--- Flask Server/app.py
+++ Flask Server/app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import requests
+import base64
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import base64
+import io
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Test Response</p>"
+@app.route("/graph-data", methods = [ 'POST'])
+def graph_data():
+    payload = json.loads(request.data)
+    b64 = requests.post("https://af-cargo-api-cargo.azuremicroservices.io/api/submit",json=payload)
+    b64 = b64.json()
+
+    graph=requests.post("http://localhost:8081/api/graph/",json=b64,timeout=(1, 5)).json()
+    y=graph["coordinates - y"]
+    x=graph["coordinates - x"]
+    i = base64.b64decode(graph["graph.png"])
+    i = io.BytesIO(i)
+    i = mpimg.imread(i, format='JPG')
+
+    # plt.imshow(i, interpolation='nearest')
+    # plt.show()
+    print(i)
+    return jsonify({"image":"test_image","coordinates":{"x":x,"y":y}})

0111ff92b8971c13f0b3ab988af111d4ce6c3e23
Ishan <ishan.kulkarni@somaiya.edu>
2022-10-16 16:51:48 +0530

    FIX: Three.js removed

--- flywise/src/App.js
+++ flywise/src/App.js
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Form from "./components/Form";
-import ModelViewer from "./components/Visualization/ModelViewer";
 import { createBrowserRouter } from "react-router-dom";
 import { useState } from "react";
 
@@ -17,9 +16,8 @@ function App() {
   return (
     <div className="font-bold">
       <Navbar />
-
       <Form result={results} setResult={setResult} />
-      {results && <ModelViewer scale="10" />}
+      {results}
       {!results && <About />}
     </div>
   );

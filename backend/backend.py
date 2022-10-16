cb9bcc0436f21107b5ae602f15b26f616be55b58
shadyskies <mehta.naman328@gmail.com>
2022-10-16 15:50:00 +0530

    backend results api

---
+++ backend/backend.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, Response
+from utils import calculate_everything
+from flask import jsonify
+import json
+from flask_cors import CORS, cross_origin
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"value": "pong"}), 200
+
+@app.route("/result", methods=["POST"])
+def _get_result():
+    data = request.json
+    print(request.json)
+    # data = json.dumps(data)
+    # data = json.loads(data)
+    # print(data)
+    if "compartment_conf" not in data:
+        return Response(status=400)
+
+    # get the result based on the compartment_conf
+    compartment_conf = data['compartment_conf']
+    data = calculate_everything(compartment_conf)
+    return jsonify(data), 200
+
+# @app.route("/", methods=["POST"])
+# def get_results(request):
+    
+if __name__ == "__main__":
+    app.config['CORS_RESOURCES'] = {r"*": {"origins": "*"}}
+    app.run(debug=True)
